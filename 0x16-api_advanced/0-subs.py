0680358e901339f53857c4d176f082caff8a9f9d
Anas <anas.001falaki@gmail.com>
2024-04-11 15:59:28 +0000

    Readme & Task1: How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module provides a function to query the Reddit API and return the number of subscribers for a given subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Retrieves the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
+
+# Testing the function
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print(number_of_subscribers(sys.argv[1]))
